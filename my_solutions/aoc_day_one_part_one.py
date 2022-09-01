0364a90ac2b852b852c9b34add4abfc3217a42cd
Julia <mazakovajulia@gmail.com>
2022-09-01 16:11:02 +0200

    Minor changes

--- my_solutions/aoc_day_one_part_one.py
+++ my_solutions/aoc_day_one_part_one.py
@@ -1,13 +1,28 @@
+import time
+import datetime
 #How many times have value increased in comparison with the previous one ?
+# my_function
+def increased(values):
+    count = 0
+    for i in range(1,len(values)):
+        if(int(values[i-1])<int(values[i])):
+            count+=1
+    return count
 
-# parsing input
-my_input = open('inputs/input_1_1.txt','r')
-values = my_input.read().splitlines()
+def main():
+    # parsing input
+    my_input = open('inputs/input_1_1.txt','r')
+    values = my_input.read().splitlines()
+    print(increased(values))
 
-count = 0
+if __name__ == "__main__":
+    st = datetime.datetime.now()
+    main()
+    # get the end datetime
+    et = datetime.datetime.now()
 
-# actual code
-for i in range(1,len(values)):
-    if(int(values[i-1])<int(values[i])):
-        count+=1
-print(count)
+    # get execution time
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
+
+
--- my_solutions/aoc_day_one_part_two.py
+++ my_solutions/aoc_day_one_part_two.py
@@ -1,11 +1,27 @@
 # How many times group of three values increased in comparison to the previous one ?
+import time
+import datetime
 
-#parsing
-my_input = open('inputs/input_1_1.txt','r')
-values = my_input.read().splitlines()
-count = 0
+# my_function
+def increased_3group(values):
+    count = 0
+    for i in range (len(values) - 3 ):
+        if ((int(values[i]) + int(values[i+1]) + int (values[i+2])) < (int(values[i+1])+int(values[i+2])+int(values[i+3]))):
+            count+=1
+    return count
 
-for i in range (len(values) - 3 ):
-    if ((int(values[i]) + int(values[i+1]) + int (values[i+2])) < (int(values[i+1])+int(values[i+2])+int(values[i+3]))):
-        count+=1
-print(count)
+def main():
+    # parsing input
+    my_input = open('inputs/input_1_1.txt','r')
+    values = my_input.read().splitlines()
+    print(increased_3group(values))
+
+if __name__ == "__main__":
+    st = datetime.datetime.now()
+    main()
+    # get the end datetime
+    et = datetime.datetime.now()
+
+    # get execution time
+    elapsed_time = et - st
+    print('Execution time:', elapsed_time, 'seconds')
